85897afde81dd3e0f58efadf070db831b79e13dd
Prince Ita <princeIta@outlook.com>
2021-07-12 15:34:59 +0100

    make stats update

--- src/app/app.model.js
+++ src/app/app.model.js
@@ -23,12 +23,12 @@ export const accounts = [
 ]
 
 export const accountStatement = [
-    { field: "Pads donated", value: 40 },
-    { field: "Cash donated", value: "₦30,000" },
-    { field: "Pads given out", value: 18 },
-    { field: "Pads available", value: 22 },
-    { field: "Number of donors", value: 1 },
-    { field: "Number of pads bought", value: 1 },
+    { field: "Pads donated", value: 0 },
+    { field: "Cash donated", value: "₦33,000" },
+    { field: "Pads given out", value: 47 },
+    { field: "Pads available", value: 7 },
+    { field: "Number of donors", value: 2 },
+    { field: "Number of pads bought", value: 54 },
     { field: "Expenses", value: "₦20,200" }
 ]
 
